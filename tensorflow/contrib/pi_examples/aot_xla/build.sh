36f323da8dae991b6a1f7fc29260d345df1f7d24
Timothy Higinbottom <timhigins@gmail.com>
2017-02-20 12:20:44 -0500

    Update build.sh

--- tensorflow/contrib/pi_examples/aot_xla/build.sh
+++ tensorflow/contrib/pi_examples/aot_xla/build.sh
@@ -1,6 +1,14 @@
 # Uses Tensorflow XLA to build an Ahead-Of-Time binary for ImageNet for Raspberry Pi
 # Usage:
-# ./build.sh <RaspberryPi Model Number>
+# ./build.sh <RaspberryPi Model Number> <ImgNet Graph Location>
+
+if [ "$2" == "" ]
+then
+  wget https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
+  unzip inception5h.zip
+  mv inception5h/tensorflow_inception_graph.pb ./imgnet.pb
+else
+  mv $2 ./imgnet.pb
 
 function build3 () {
   tfcompile --graph=imgnet.pb --config=imgnet_config.pbtxt --target_triple=arm-v8-unknown-linux-gnueabi --target_cpu=cortex-a53 --target_features=neon-fp-armv8
