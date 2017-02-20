ba60cd9b10c4c12e4c39ce9e39eb5da2359c3ab6
Timothy Higinbottom <timhigins@gmail.com>
2017-02-20 12:40:41 -0500

    Update build.sh

--- tensorflow/contrib/pi_examples/aot_xla/build.sh
+++ tensorflow/contrib/pi_examples/aot_xla/build.sh
@@ -4,11 +4,12 @@
 
 if [ "$2" == "" ]
 then
-  wget https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
-  unzip inception5h.zip
-  mv inception5h/tensorflow_inception_graph.pb ./imgnet.pb
+  curl https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip --output inception.zip
+  unzip inception.zip inception
+  mv inception/tensorflow_inception_graph.pb ./imgnet.pb
 else
   mv $2 ./imgnet.pb
+fi
 
 function build3 () {
   tfcompile --graph=imgnet.pb --config=imgnet_config.pbtxt --target_triple=arm-v8-unknown-linux-gnueabi --target_cpu=cortex-a53 --target_features=neon-fp-armv8
@@ -18,6 +19,6 @@ case "$1" in
     0) echo "RPi Zero not yet supported";;
     1) echo "Coming Soon";;
     2) echo "Coming Soon";;
-    3) build3
+    3) build3;;
     *) echo "Unknown model";;
 esac
